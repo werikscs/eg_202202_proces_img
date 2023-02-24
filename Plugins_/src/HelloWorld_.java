eaf8c8bae033ff62c4b23c022e07e59cc3122fe5
Weriks Santos <werikscs@gmail.com>
2023-02-24 16:48:00 -0300

    finish config

--- Plugins_/src/HelloWorld_.java
+++ Plugins_/src/HelloWorld_.java
@@ -1,7 +1,7 @@
 import ij.IJ;
 import ij.plugin.PlugIn;
  
-public class Another_Plugin  implements PlugIn {
+public class HelloWorld_  implements PlugIn {
 	public void run(String arg) {
 		IJ.error("Hello world!");
 	}
--- TESTPlugin_/src/TestPlugin_.java
+++
@@ -1,8 +0,0 @@
-import ij.IJ;
-import ij.plugin.PlugIn;
- 
-public class TestPlugin_  implements PlugIn {
-	public void run(String arg) {
-		IJ.error("Hello world!");
-	}
-}
